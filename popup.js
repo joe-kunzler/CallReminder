5fc5bc31ddfdcd63e07bf5583a21c7d90aa1b93c
Joseph Kunzler <joseph.kunzler@protonmail.com>
2023-04-02 18:01:35 -0400

    Reminders

--- popup.js
+++ popup.js
@@ -1,54 +1,53 @@
-// Popup script code here
-
-// Get the reminder list element
-const reminderList = document.getElementById("reminder-list");
-
-// Load the reminders from storage
-chrome.storage.sync.get("reminders", function (data) {
-  if (data.reminders) {
-    // Iterate over each reminder and add it to the list
-    for (const reminder of data.reminders) {
-      const li = document.createElement("li");
-      li.textContent = `${reminder.name} - ${reminder.frequency} - ${reminder.date}`;
-      reminderList.appendChild(li);
-    }
+function addReminderToList(reminder) {
+    const reminderList = document.getElementById("reminder-list");
+    const listItem = document.createElement("li");
+    const text = document.createTextNode(`${reminder.name} - ${reminder.frequency}`);
+    listItem.appendChild(text);
+    reminderList.appendChild(listItem);
   }
-});
-
-// Listen for the form submit event
-document.querySelector("form").addEventListener("submit", function (event) {
-  // Prevent the form from submitting
-  event.preventDefault();
-
-  // Get the form values
-  const name = document.getElementById("name").value;
-  const frequency = document.getElementById("frequency").value;
-  const customFrequency = document.getElementById("custom_frequency").value;
-  const date = document.getElementById("date").value;
-
-  // Create the reminder object
-  const reminder = {
-    name,
-    frequency: frequency === "custom" ? customFrequency : frequency,
-    date
-  };
-
-  // Get the reminders from storage
-  chrome.storage.sync.get("reminders", function (data) {
-    let reminders = data.reminders || [];
-
-    // Add the new reminder to the list
-    reminders.push(reminder);
-
-    // Save the reminders to storage
-    chrome.storage.sync.set({ reminders }, function () {
-      // Clear the form
-      document.querySelector("form").reset();
-
-      // Add the new reminder to the list
-      const li = document.createElement("li");
-      li.textContent = `${reminder.name} - ${reminder.frequency} - ${reminder.date}`;
-      reminderList.appendChild(li);
+  
+  function loadReminders() {
+    chrome.storage.sync.get("reminders", function (data) {
+      if (data.reminders) {
+        for (const reminder of data.reminders) {
+          addReminderToList(reminder);
+        }
+      }
     });
+  }
+  
+  function addReminder(event) {
+    event.preventDefault();
+  
+    const name = document.getElementById("name").value;
+    const frequency = document.getElementById("frequency").value;
+    const customFrequency = document.getElementById("custom_frequency").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    const id = Date.now().toString();
+  
+    const reminder = {
+      id,
+      name,
+      frequency,
+      customFrequency,
+      date,
+      time
+    };
+  
+    chrome.storage.sync.get("reminders", function (data) {
+      const reminders = data.reminders || [];
+      reminders.push(reminder);
+      chrome.storage.sync.set({ reminders }, function () {
+        addReminderToList(reminder);
+      });
+    });
+  
+    event.target.reset();
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    loadReminders();
+    document.getElementById("add-reminder-form").addEventListener("submit", addReminder);
   });
-});
+  
