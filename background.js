a02d353b663ff4d24a8a4c871b594b44dfee4633
Joseph Kunzler <joseph.kunzler@protonmail.com>
2023-04-02 17:07:34 -0400

    Adding notifiactions

--- background.js
+++ background.js
@@ -1,1 +1,49 @@
-// Background script code here
+function checkReminders() {
+    chrome.storage.sync.get("reminders", function (data) {
+      if (data.reminders) {
+        for (const reminder of data.reminders) {
+          const now = new Date();
+          const date = new Date(reminder.date);
+          const frequency = reminder.frequency;
+          let nextDate;
+  
+          switch (frequency) {
+            case "daily":
+              nextDate = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+              break;
+            case "weekly":
+              nextDate = new Date(date.getTime() + 7 * 24 * 60 * 60 * 1000);
+              break;
+            case "biweekly":
+              nextDate = new Date(date.getTime() + 14 * 24 * 60 * 60 * 1000);
+              break;
+            case "monthly":
+              nextDate = new Date(date.getTime());
+              nextDate.setMonth(nextDate.getMonth() + 1);
+              break;
+            case "custom":
+              nextDate = new Date(date.getTime() + parseInt(reminder.customFrequency) * 24 * 60 * 60 * 1000);
+              break;
+            default:
+              nextDate = null;
+          }
+  
+          if (nextDate && now >= date && now <= nextDate) {
+            const options = {
+              type: "basic",
+              title: "Call Reminder",
+              message: `It's time to call ${reminder.name}!`,
+              iconUrl: "icon128.png"
+            };
+  
+            chrome.notifications.create(options);
+          }
+        }
+      }
+    });
+  }
+
+// Call the checkReminders function every 15 minutes
+setInterval(checkReminders, 15 * 60 * 1000);
+
+  
